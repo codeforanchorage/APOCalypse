520519f58ccc98082d71e3e7e2382b4d1f1768de
Scott Pustay <scott@socure.com>
2015-09-23 13:53:55 -0800

    cleaning up comments

--- feeder.py
+++ feeder.py
@@ -7,10 +7,8 @@ from config import Config
 import boto.sqs
 
 def main():
-    # Start at https://aws.state.ak.us/ApocReports/CampaignDisclosure/View.aspx?ID=1000
-    # Go to 
-    # https://aws.state.ak.us/ApocReports/CampaignDisclosure/View.aspx?ID=13050
-    print 'feeder'
+
+    # Gather AWS credentials
     f = file('/Users/spustay/.aws/cfa_credentials_python_module_Config')
     cfg = Config(f)
     for c in cfg.credentials:
@@ -19,12 +17,11 @@ def main():
         _secret = c.aws_secret_access_key 
 
     conn = boto.sqs.connect_to_region(_region,aws_access_key_id=_access,aws_secret_access_key=_secret)
-    #all_queues = conn.get_all_queues()
-    #for a in all_queues:
-    #    print a
 
     feeder_q = conn.get_queue('APOCalypse_feeder')
 
+    # Start at https://aws.state.ak.us/ApocReports/CampaignDisclosure/View.aspx?ID=1000
+    # & go to  https://aws.state.ak.us/ApocReports/CampaignDisclosure/View.aspx?ID=13050
     for x in range(1000, 13050):
         print str(x)
         payload = "https://aws.state.ak.us/ApocReports/CampaignDisclosure/View.aspx?ID=" + str(x)
